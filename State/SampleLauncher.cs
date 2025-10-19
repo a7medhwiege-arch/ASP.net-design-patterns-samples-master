907a0e1560dbe14f1f306505641eac83ef8e4c14
a7medhwiege-arch <a7medhwiege@gmail.com>
2025-10-20 02:06:58 +0300

    Add files via upload

---
+++ State/SampleLauncher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using State.Impl;
+
+namespace State
+{
+	internal class SampleLauncher
+	{
+		static void Main(string[] args)
+		{
+			Transaction transaction = new Transaction();
+			
+			transaction.StartTransaction();
+			Console.WriteLine($"Transaction state: {transaction.TransactionState}");
+			
+			transaction.SetSum(250);
+			Console.WriteLine($"Transaction state: {transaction.TransactionState}");
+			
+			transaction.SetReceiver("9984-3345-2211-2365-2234");
+			Console.WriteLine($"Transaction state: {transaction.TransactionState}");
+			
+			transaction.CompleteTransaction();
+			Console.WriteLine($"Transaction state: {transaction.TransactionState}");
+
+			Console.ReadKey();
+		}
+	}
+}
